7a29e50a0ff78794eff4697cfaca932c5c10cd65
Changmin Lee <geochangmin@gmail.com>
2026-03-23 04:04:03 +0000

    버튼을 사용한 과목 선택 기능 제거: 전공핵심 및 전공심화 과목을 텍스트로 표시

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,12 @@ body {
 st.title("2026학년도 1학기 인천대학교 사범대학 수학교육과 교과목 소개")
 
 st.header("전공핵심")
-if st.button("수학교과교육론"):
-    st.switch_page("pages/1_수학교과교육론.py")
-if st.button("해석학"):
-    st.switch_page("pages/2_해석학.py")
+st.write("수학교과교육론")
+st.write("해석학")
 
 st.header("전공심화")
-if st.button("미분방정식"):
-    st.switch_page("pages/3_미분방정식.py")
-if st.button("조합및그래프이론"):
-    st.switch_page("pages/4_조합및그래프이론.py")
-if st.button("집합론"):
-    st.switch_page("pages/5_집합론.py")
+st.write("미분방정식")
+st.write("조합및그래프이론")
+st.write("집합론")
+
+st.write("각 교과목 페이지는 왼쪽 사이드바에서 선택할 수 있습니다.")
