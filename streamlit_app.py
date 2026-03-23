a2db7dd0e80085b0ef67c4f4329fa87721c877a7
Changmin Lee <geochangmin@gmail.com>
2026-03-23 04:02:29 +0000

    버튼을 사용하여 과목 선택 기능 추가: 전공핵심 및 전공심화 과목에 대해 페이지 링크를 버튼으로 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,15 @@ body {
 st.title("2026학년도 1학기 인천대학교 사범대학 수학교육과 교과목 소개")
 
 st.header("전공핵심")
-st.page_link("1_수학교과교육론", label="수학교과교육론")
-st.page_link("2_해석학", label="해석학")
+if st.button("수학교과교육론"):
+    st.switch_page("pages/1_수학교과교육론.py")
+if st.button("해석학"):
+    st.switch_page("pages/2_해석학.py")
 
 st.header("전공심화")
-st.page_link("3_미분방정식", label="미분방정식")
-st.page_link("4_조합및그래프이론", label="조합및그래프이론")
-st.page_link("5_집합론", label="집합론")
+if st.button("미분방정식"):
+    st.switch_page("pages/3_미분방정식.py")
+if st.button("조합및그래프이론"):
+    st.switch_page("pages/4_조합및그래프이론.py")
+if st.button("집합론"):
+    st.switch_page("pages/5_집합론.py")
