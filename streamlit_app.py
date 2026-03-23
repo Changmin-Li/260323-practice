7b28859c856b14c351778e7e9b0504be57c97670
Changmin Lee <geochangmin@gmail.com>
2026-03-23 04:00:29 +0000

    페이지 링크 경로 수정: 전공핵심 및 전공심화 과목 링크에서 'pages/' 제거

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ body {
 st.title("2026학년도 1학기 인천대학교 사범대학 수학교육과 교과목 소개")
 
 st.header("전공핵심")
-st.page_link("pages/1_수학교과교육론.py", label="수학교과교육론")
-st.page_link("pages/2_해석학.py", label="해석학")
+st.page_link("1_수학교과교육론", label="수학교과교육론")
+st.page_link("2_해석학", label="해석학")
 
 st.header("전공심화")
-st.page_link("pages/3_미분방정식.py", label="미분방정식")
-st.page_link("pages/4_조합및그래프이론.py", label="조합및그래프이론")
-st.page_link("pages/5_집합론.py", label="집합론")
+st.page_link("3_미분방정식", label="미분방정식")
+st.page_link("4_조합및그래프이론", label="조합및그래프이론")
+st.page_link("5_집합론", label="집합론")
