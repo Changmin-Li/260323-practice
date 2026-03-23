3ce465a336a3ddb0ff88eb0eebb7669a58006165
Changmin Lee <geochangmin@gmail.com>
2026-03-23 04:11:09 +0000

    페이지 레이아웃 설정 추가: 스트림릿 페이지 설정에 레이아웃 옵션 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="2026학년도 1학기 인천대학교 사범대학 수학교육과 교과목 소개", page_icon="📚")
+st.set_page_config(page_title="2026학년도 1학기 인천대학교 사범대학 수학교육과 교과목 소개", page_icon="📚", layout="wide")
 
 st.markdown("""
 <style>
